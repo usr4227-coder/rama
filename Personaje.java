13f8fbb581a5d3939834deb2efa74bcd03424b48
Nick <usr4227@salesianos-lacuesta.net>
2026-03-31 17:51:48 +0100

    Crear setters y getters

--- Personaje.java
+++ Personaje.java
@@ -12,4 +12,29 @@ public class Personaje {
         this.energia = 100;
         totalPersonajes++;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
+
 }
