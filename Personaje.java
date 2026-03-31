70024f78db6978aabf0e684e3dd7f044938b5bc0
Nick <usr4227@salesianos-lacuesta.net>
2026-03-31 18:05:43 +0100

    Update

--- Personaje.java
+++ Personaje.java
@@ -13,28 +13,4 @@ public class Personaje {
         totalPersonajes++;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getNivel() {
-        return nivel;
-    }
-
-    public int getEnergia() {
-        return energia;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setNivel(int nivel) {
-        this.nivel = nivel;
-    }
-
-    public void setEnergia(int energia) {
-        this.energia = energia;
-    }
-
 }
