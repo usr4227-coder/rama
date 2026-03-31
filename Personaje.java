2163c1dd242d7037926d9935be5fc56cbe058351
Nick <usr4227@salesianos-lacuesta.net>
2026-03-31 17:50:43 +0100

    Crear clase personajes

---
+++ Personaje.java
@@ -0,0 +1,15 @@
+
+public class Personaje {
+
+    private String nombre;
+    private int nivel;
+    protected int energia;
+    public static int totalPersonajes = 0;
+
+    public Personaje(String nombre, int nivel) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.energia = 100;
+        totalPersonajes++;
+    }
+}
